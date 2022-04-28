e6923f9f68a7f938dc58ebe7b720a93074e1425c
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-04-28 11:29:32 +0100

    Feature: Commit de los métodos aleatorios

--- src/com/company/Producto.java
+++ src/com/company/Producto.java
@@ -1,5 +1,25 @@
 package com.company;
 
-public class Producto {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class Producto {
+    public String producto;
+    public String precio;
+
+    public Producto(String producto, String precio) {
+        this.producto = producto;
+        this.precio = precio;
+    }
+    public String getProducto() {
+        return producto;
+    }
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+    public String getPrecio() {
+        return precio;
+    }
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
 }
